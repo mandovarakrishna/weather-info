a335a657aca5aebed6ec37ef539ed8c7caaecd66
Krishna Mandovara <kmandovarakrishna@gmail.com>
2020-12-05 13:17:25 +0530

    adding ignore

--- src/main/java/com/example/weather/model/Main.java
+++ src/main/java/com/example/weather/model/Main.java
@@ -1,7 +1,9 @@
 package com.example.weather.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Main {
 
 	@JsonProperty("temp")
